15783c23573375e674e6d02a96df0e5e24c177fd
Joe Filby <jmfilby@gmail.com>
2022-03-04 15:19:37 +0000

    final version?

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
-'''
-what is needed:
-
--deck
--player(s) - human
-  -has account
-  - bets on a hand
-  - gets 2 cards face up
--dealer
-  - gets 2 cards - one 'face up', one 'face down'
-'''
-
-'''VARIABLES'''
-
 import random
+
 value = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,   'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-player_value = 0
-dealer_value = 0
+player_val = 0
+dealer_val = 0
 game_on = True
 
+
 '''CLASSES'''
+
+
+class Card:
+
+  def __init__(self, suit, rank):
+    self.suit = suit
+    self.rank = rank
+    self.value = value[rank]
+
+  def __lt__(self, other):
+         return self.value < other.value
+
+  
+  def __str__(self):
+    return self.rank +" of " + self.suit + " valued at " + f"{self.value}"
+
+
+class Deck:
+
+  def __init__(self):
+    self.all_cards = []
+
+    for suit in suits:
+      for rank in ranks:
+        created_card = Card(suit,rank)
+        self.all_cards.append(created_card)
+
+  def shuffle(self):
+    random.shuffle(self.all_cards)
+
+  def draw_one(self):
+    return self.all_cards.pop()
+
 
 class Player:
   
@@ -32,130 +53,208 @@ class Player:
   def __str__():
     return f"{self.name}, your balance is {self.acc}"
 
-  def add_one(new_card):
+  def add_one(self,new_card):
     self.hand.append(new_card)
 
-class Deck:
-
-  def __init__(self):
-    self.all_cards = []
-
-  def shuffle():
-    random.shuffle(self.all_cards)
-
-  def draw_one():
-    return self.all_cards.pop()
+  def adjust_bal(self, bet, result,):
+    if result == "plus":
+      return self.acc + (bet*2)
+    elif result == "blackjack":
+      return self.acc + (bet*2.5)
+    elif result == "draw":
+      return self.acc + bet
+    else:
+      return self.acc - bet
 
 class Dealer:
   def __init__(self):
     self.hand = []
 
-  def add_one(new_card):
+  def add_one(self,new_card):
     self.hand.append(new_card)
 
 
-
 ''' METHODS '''
-def player_value(player_value, cards):
-  player_value = 0
-  for x in cards:
-    if x.value == 11:
-      if value + 11 < 22:
-        player_value += x.value
-    else: 
-      player_value += 1
-  else:
-    player_value += x.value
+
+
+def new_screen():
+  print ("\n" * 15)
   
-  return player_value 
-
-def dealer_turn(dealer_value, cards):
-  dealer_value = 0
-  for x in cards:
+def player_value(player_val, cards):
+  player_val = 0
+  sorted_cards = sorted(cards)
+  for x in sorted_cards:
     if x.value == 11:
-      if value + 11 < 22:
-        dealer_value += x.value
-    else: 
-      dealer_value += 1
-  else:
-    dealer_value += x.value
+      if player_val + 11 < 22:
+        player_val += x.value
+      else: 
+        player_val += 1
+    else:
+      player_val += x.value
   
-  return player_value
+  return player_val 
+
+def dealer_value(dealer_val, cards):
+  dealer_val = 0
+  sorted_cards = sorted(cards)
+  for x in sorted_cards:
+    if x.value == 11:
+      if dealer_val + 11 < 22:
+        dealer_val += x.value
+      else: 
+        dealer_val += 1
+    else:
+      dealer_val += x.value
+  
+  return dealer_val
+
+def another_round(game_on):
+  ask = input("do you want to play another round? y or n")
+  if ask == "y":
+    print ("OK let's go again..")
+    game_on = True
+    print (game_on)
+  else:
+    print (f"thanks for playing, you leave the table with {player_one.acc} in your account")
+    game_on = False
+
+  return game_on
 
 
-'''game setup'''
+'''GAME SETUP'''
+
+
 name = input("Hello, what is your name? ")
 player_one = Player(name)
 dealer_one = Dealer()
-deck_one = Deck()
-game_on = True
-'''Logic'''
 
+game_on = True
+result = ""
+
+
+'''LOGIC'''
+
+
+print (f"ok {player_one.name}, lets play BLACKJACK")
 
 while game_on == True:
-  print (f"ok {player_one.name}, lets play BLACKJACK")
-  print (f"your account balance is {player_one.acc}") 
+  deck_one = Deck()
+  deck_one.shuffle()
+  valid = False
+  player_one.hand = []
+  dealer_one.hand = []
   
-  bet = int(input(f"{player_one.name} how much do you want to bet this round? enter a number"))
-   
+  print (f"your account balance is {player_one.acc}") 
+
+  while valid == False:
+    bet = int(input(f"{player_one.name} how much do you want to bet this round? enter a number:\n"))
+    if bet > player_one.acc:
+      print ("that's not a valuid bet")
+      valid = False
+    else:
+      valid = True
+      
+  result = "minus"
+  player_one.acc = player_one.adjust_bal(bet, result)
+
+  new_screen()
+  print (f"you bet {bet} and your remaining balance is {player_one.acc}")
+  print("let's play, the dealer starts to deal...")
+
+
+
   for x in range(2):
     player_one.add_one(deck_one.draw_one())
     dealer_one.add_one(deck_one.draw_one())
-    player_value(player_value, player_one.hand)
-    dealer_value(dealer_value, dealer_one.hand)
+
+  player_val = player_value(player_val, player_one.hand)
+  dealer_val = dealer_value(dealer_val, dealer_one.hand)
   
-  p
-
   
+  print ("In your hand you have:")
+  for x in player_one.hand:    
+    print (x)
+  
+  print(f"you have {player_val}")
 
-
-
-
-''''
-  game end checks:
-
-  player gets 21: black jack
-  player gets over 21: bust 
-
-  dealer get 21: dealer Wins
-  dealer gets over 21: bust
-
-
-dealer hit >16 but < player total : player Wins
-dealer > player total <22: dealer wins
-
-
-
-logic
-
-create deck
-create player (ask name, assign bank balance)
-create dealer (comp)
-
-player bets
-
-deal two cards to player and dealer
-display player cards
-display 1 dealer card (0)
-
-ask player to take cards until:
-- they decline
-- total = 21 - black jack, break
-- total > 21 - bust, break
-
-dealer take card if total < 16.
-
-stops if total >16 <22
-stops if total > 21
-wins if total > player total and less than 22 break
-loses if total < player total
-
-if player wins, bet is returned x2 and added to acc
-
-ask to play another hand:
-
-loop
-
-
-
-'''
+  if player_val == 21:
+    print ("BLACKJACK! You win BIG!")
+    result = "blackjack"
+    player_one.acc = player_one.adjust_bal(bet, result)
+    print (f"your account balance is now {player_one.acc}")
+    game_on = another_round(game_on)
+    break
+  else:
+    action = input("do you want to stick or twist? enter t or s")
+    
+    while action == "t":
+      player_one.add_one(deck_one.draw_one())
+      print (f"you got {player_one.hand[-1]}")
+      player_val = player_value(player_val, player_one.hand)
+      
+      if player_val >=22:
+        print (f"You have {player_val}")
+        print ("you are bust, you lose this hand")
+        print (f"your account balance is now {player_one.acc}")
+        if player_one.acc > 0:
+          game_on = another_round(game_on)
+        else:
+          print ("your are broke! Get lost loser!")
+          game_on = False
+          break
+        break
+      elif player_val == 21:
+        print ("21! you stick")
+        break
+      else:
+        print (f"you have {player_val}")
+        action = input("do you want to stick or twist? enter t or s")
+        
+    if player_val < 22:
+      new_screen()
+      print ("now it's the dealer's turn...")
+      print (f"the dealer has in their hand:")
+      for x in dealer_one.hand:    
+        print (x)
+    
+      print (f"the dealer has {dealer_val}")
+  
+      if dealer_val >= 16:
+        pass
+      else:
+        while dealer_val < 16:
+          dealer_one.add_one(deck_one.draw_one())
+          dealer_val = dealer_value(dealer_val, dealer_one.hand)  
+          print (f"the dealer deals themself a {dealer_one.hand[-1]} and now has {dealer_val}")
+        
+      if dealer_val > 22:
+        result = "plus"
+        player_one.acc = player_one.adjust_bal(bet, result)
+        print ("the dealer has bust! you win this hand")
+        print (f"You win {bet} and your new account balance is {player_one.acc}")
+        
+    
+      elif dealer_val >= 16 and dealer_val == player_val:
+        result = "draw"
+        player_one.acc = player_one.adjust_bal(bet, result)
+        print ("its a draw! you get you money back")
+        print (f"Your account balance is {player_one.acc}")
+       
+    
+      elif dealer_val >= 16 and dealer_val < player_val:
+        result = "plus"
+        player_one.acc = player_one.adjust_bal(bet, result)
+        print (f"The dealer sticks on {dealer_val}. You have {player_val}, you win!")
+        print (f"Your account balance is {player_one.acc}")
+                                              
+    
+      elif dealer_val >= 16 and dealer_val > player_val:
+        print (f"The dealer sticks on {dealer_val}. You have {player_val}, you lose :(")
+        print (f"Your account balance is {player_one.acc}")
+        
+    
+  if player_one.acc > 0:
+    game_on = another_round(game_on)
+  else:
+    print ("your are broke! Get lost loser!")
+    game_on = False
